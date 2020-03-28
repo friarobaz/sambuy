da03286f23c9158a542458edb289fa8e7635f79e
friarobaz <59922807+friarobaz@users.noreply.github.com>
2020-03-28 15:52:33 +0100

    fade image

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var WINS_MAX = 2;
+var WINS_MAX = 3;
 
 var questions = [
     "qui a fait 9c",
@@ -13,7 +13,7 @@ var answers = [
     "SHARMA",
     "HONNOLD"];
 
-var categories = [
+var categories = [  //not used yet
     "people",
     "people",
     "people"];
@@ -21,7 +21,7 @@ var categories = [
 var picked = [];
 var guess = "";
 var nb_of_wins = 0;
-var random_number = Math.floor( Math.random() * nb_of_questions);
+var random_number = 0;
 var the_question = "";
 var the_answer = "";
 
@@ -30,37 +30,55 @@ function display_question(){
     document.getElementById("image_box").innerHTML = "<img src=img/" + the_answer + ".jpg>";
 };
 
+function fade_image() {
+    document.getElementById("image_box").style.opacity = 0;
+    $( "#image_box" ).animate({opacity: 1}, 10000,);
+  };
+
 function initialize() {
     //reset CSS
     document.getElementById("guess_box").style.backgroundColor = "inherit";
-    document.getElementById("next").style.visibility = "hidden";
-    document.getElementById("start_over").style.visibility = "hidden";
-    document.getElementById("nb_of_wins_box").innerHTML = nb_of_wins;
+    document.getElementById("next").style.display = "none";
+    document.getElementById("start_over").style.display = "none";
+    document.getElementById("nb_of_wins").innerHTML = nb_of_wins;
     //pick unpicked random question 
+    random_number = Math.floor( Math.random() * nb_of_questions);
     while (picked[random_number]){
         random_number = Math.floor( Math.random() * nb_of_questions);
     }
     the_question = questions[random_number];
     the_answer = answers[random_number];
     guess = "";
+    document.getElementById("guess_box").innerHTML = guess;
     display_question();
-  }
+    fade_image();
+  };
 
 function start_over() {
     nb_of_wins = 0;
     picked = new Array(nb_of_questions).fill(false);
+    document.getElementById("nb_of_wins_box").style.display = "block";
+    document.getElementById("question_box").style.display = "block";
+    document.getElementById("guess_box").style.display = "block";
+    document.getElementById("image_box").style.display = "block";
     initialize();
 };
 
 function win_question(){
+    $("#image_box").stop();
+    document.getElementById("image_box").style.opacity = 1;
     document.getElementById("guess_box").style.backgroundColor = "green"; //set CSS
     picked[random_number] = true; //mark question as picked
     nb_of_wins ++; //inscrease nb_of_wins
-    document.getElementById("nb_of_wins_box").innerHTML = nb_of_wins; //display nb_of_wins
+    document.getElementById("nb_of_wins").innerHTML = nb_of_wins; //display nb_of_wins
     if (nb_of_wins < WINS_MAX){
-        document.getElementById("next").style.visibility = "visible"; //set CSS
+        document.getElementById("next").style.display = "block"; //no done yet
     } else {
-        document.getElementById("start_over").style.visibility = "visible"; //set CSS
+        document.getElementById("question_box").style.display = "none";
+        document.getElementById("image_box").style.display = "none";
+        document.getElementById("nb_of_wins_box").style.display = "none";
+        document.getElementById("guess_box").style.display = "none";
+        document.getElementById("start_over").style.display = "block"; //end game
     };
 };
 
@@ -70,9 +88,10 @@ start_over();
 
 //get and display guess
 $(document).keydown(function(event){ //get guess
-    if (guess != the_answer){
-        var letter_pressed = String.fromCharCode(event.which); //get letter
-        var key_pressed = event.which; //get key
+    var letter_pressed = String.fromCharCode(event.which); //get letter
+    var key_pressed = event.which; //get key
+
+    if (guess != the_answer){ //if not right answer yet
 
         if (key_pressed == 8 && guess.length > 0){ //if backspace
             guess = guess.slice(0, -1); //remove last letter
@@ -81,11 +100,12 @@ $(document).keydown(function(event){ //get guess
         };
 
         document.getElementById("guess_box").innerHTML = guess; //display guess
-
         if (guess == the_answer){
             win_question();
         };
-    }; //end if guess != answer
+    }else if (key_pressed == 13 && nb_of_wins < WINS_MAX){
+        initialize();
+    };
     
 }); //end get guess
 
